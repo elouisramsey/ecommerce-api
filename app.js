90587121c07fe6fecb23bd6215f11dd988790582
unknown <louis.ezzim@crowdforce.io>
2021-09-25 15:10:38 +0100

    changes to app.js

--- app.js
+++ app.js
@@ -1,24 +1,30 @@
-const createError = require('http-errors');
-const express = require('express');
-const path = require('path');
+const createError = require('http-errors')
+const express = require('express')
+const path = require('path')
 const cors = require('cors')
 const helmet = require('helmet')
-const cookieParser = require('cookie-parser');
+const cookieParser = require('cookie-parser')
 const expressSession = require('express-session')
-const logger = require('morgan');
+const logger = require('morgan')
 const mongoose = require('mongoose')
 require('dotenv').config()
 const PORT = process.env.PORT || 7000
 
-const indexRouter = require('./routes/index');
-const MongoStore = require('connect-mongo')(session)
-const app = express();
-const session = {
-  secret: process.env.SESSION_SECRET,
-  cookie: {},
-  resave: false,
-  saveUninitialized: false
-}
+const indexRouter = require('./routes/index')
+const MongoStore = require('connect-mongo')
+const app = express()
+
+app.use(
+  expressSession({
+    secret: process.env.SESSION_SECRET,
+    resave: true,
+    saveUninitialized: true,
+    store: MongoStore.create({
+      mongoUrl: process.env.SESSION
+    })
+  })
+)
+
 // Connect DB
 mongoose
   .connect(process.env.ATLAS_URI)
@@ -30,23 +36,29 @@ mongoose
   )
   .catch((err) => console.log(err))
 
-app.use(logger('dev'));
-app.use(express.json());
+app.use(logger('dev'))
+app.use(express.json())
 app.use(cors())
-app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
+app.use(express.urlencoded({ extended: false }))
+app.use(cookieParser())
 app.use(helmet())
-app.use(express.static(path.join(__dirname, 'pages')));
+app.use(express.static(path.join(__dirname, 'pages')))
+app.use(
+  expressSession({
+    secret: process.env.SESSION_SECRET,
+    resave: true,
+    saveUninitialized: true
+  })
+)
 
-app.use({ secret: process.env.SESSION_SECRET, store: new MongoStore(session) })
-app.use('/', indexRouter);
-app.use('/product', require('./routes/Product'));
-app.use('/categories', require('./routes/Category'));
+app.use('/', indexRouter)
+app.use('/product', require('./routes/Product'))
+app.use('/categories', require('./routes/Category'))
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  next(createError(404));
-});
+app.use(function (req, res, next) {
+  next(createError(404))
+})
 
 // error handler
 if (app.get('env') === 'development') {
@@ -54,13 +66,13 @@ if (app.get('env') === 'development') {
     res.status(err.status || 500)
   })
 }
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
   // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+  res.locals.message = err.message
+  res.locals.error = req.app.get('env') === 'development' ? err : {}
 
   // render the error page
-  res.status(err.status || 500);
-});
+  res.status(err.status || 500)
+})
 
-module.exports = app;
+module.exports = app
