c135e76c3c697fc324ba7ed9af76bc65660e0cd7
unknown <louis.ezzim@crowdforce.io>
2021-09-25 14:23:14 +0100

    changes to app

--- app.js
+++ app.js
@@ -36,7 +36,7 @@ app.use(cors())
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(helmet())
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'pages')));
 
 app.use(expressSession(session))
 app.use('/', indexRouter);
